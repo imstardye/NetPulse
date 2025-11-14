5a5200729df7c1039a510625d58e55eaf186eb9a
GitHub Action <action@github.com>
2025-11-14 04:48:49 +0000

    Bump version to 1.7.3 (107003)

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "1.7.2"
-            versionCode = 107002
+            versionName = "1.7.3"
+            versionCode = 107003
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
